749f2aa065a6a00b1cb870a2f3d1b0ba888cad89
ChalkPE <amato0617@gmail.com>
2015-02-23 22:26:01 +0900

    CustomPacket.js 대폭 수정

--- ModPE/CustomPacket.js
+++ ModPE/CustomPacket.js
@@ -1,88 +1,79 @@
-var CustomPacket = (function(){
-    /**
-     * @file   CustomPacket.js
-     * @date   2014-11-03
-     * @author ChalkPE
-     * @brief  블럭런쳐에서 실행되는 CustomPacket의 클라이언트입니다.
-     */
-    
-    "use strict";
-    
-    var PORT  = 19131; ///< 통신에 사용할 포트. PE의 기본 포트가 19132이므로 19131을 채택했습니다.
-    var DEBUG = true; ///< 디버그 모드. true이면 디버깅 메세지가 출력됩니다.
-    
-    var channel; ///< 소켓 통신에 쓰이는 객체입니다.
-    var decoder = java.nio.charset.Charset.forName("UTF-8").newDecoder(); ///< 바이트버퍼를 UTF-8로 디코딩해주는 객체입니다.
-    
-    try{
-        channel = java.nio.channels.DatagramChannel.open();
-    }catch(e){
-        debug(e.name + " - " + e.message, true);
-    }
-    
-    /**
-     * @brief  디버깅 메세지를 출력합니다.
-     * @param  message 출력할 메세지
-     * @param  force   디버그 모드가 아닐 때의 출력 여부 - 생략 가능
-     * @return 없음
-     */
-    function debug(message, force){
-        if(DEBUG || force) print(message);
-    }
-    
-    /**
-     * @brief  소켓을 닫습니다.
-     * @return 없음
-     */
-    function finalize(){
-        if(channel !== null){
-            channel.close();
-            channel = null;
-        }
-    }
-    
-    /**
-     * @brief  서버에 연결해 통신을 합니다. 예외는 내부에서 처리합니다.
-     * @param  str  보내는 문자열
-     * @param  hook 서버가 응답한 내용을 전달받을 함수
-     * @param  ip   서버의 IP 문자열
-     * @param  port 서버의 포트 번호 - 생략 가능, 기본값은 PORT
-     * @return 없음
-     */
-    function connect(str, hook, ip, port){
-        new java.lang.Thread({run: function(){
-            
-            if(channel === null){
+"use strict";
+
+/**
+ * @author ChalkPE
+ * @since 2014-11-03
+ * @see https://github.com/if-Team/CustomPacket
+ */
+var CustomPacket = {};
+CustomPacket.PORT = 19131;
+
+var channel = null;
+var decoder = null;
+var buffer = null;
+
+/**
+ * @brief  서버에 연결합니다
+ * @param  address {string} 서버의 IP
+ * @param  message {string} 서버로 보내는 문자열
+ * @param  callback {function} 서버가 응답한 내용을 전달받을 콜백 객체
+ * @return 없음
+ */
+CustomPacket.get = function get(address, message, callback){
+    new java.lang.Thread({run: function(){
+        try{
+        	if(channel === null){
                 throw new Error("channel is not initialized");
             }
-            
-            port = port || PORT;
-            
-            try{
-                var remoteAddress = new java.net.InetSocketAddress(ip, port);
-                var sendBuffer = java.nio.ByteBuffer.wrap(new java.lang.String(str).getBytes("UTF-8"));
-                var sentBytes = channel.send(sendBuffer, remoteAddress);
-                
-                debug("I SUCCESSFULLY SENT " + sentBytes + " BYTES TO " + remoteAddress.toString() + "!");
-                
-                var receiveBuffer = java.nio.ByteBuffer.allocateDirect(65507);
-                channel.receive(receiveBuffer);
-                
-                receiveBuffer.flip();
-                var read = decoder.decode(receiveBuffer).toString() + "";
-                receiveBuffer.clear();
-                
-                debug("I RECIEVED FROM SERVER! NOW CALLING HOOK...");
-                
-                hook(read);
-            }catch(e){
-                debug(e.name + " - " + e.message, true);
+        	
+        	if(address === null || message === null || callback === null){
+                throw new Error("argument cannot be null");
             }
-        }}).start();
-    }
+        	
+        	if(typeof address !== "string"){
+        		throw new Error("address must be a string");
+        	}
+        	
+        	if(typeof message !== "string"){
+        		throw new Error("message must be a string");
+        	}
+        	
+        	if(typeof callback !== "function"){
+        		throw new Error("callback must be a function");
+        	}
+        	
+            var src = java.nio.ByteBuffer.wrap(new java.lang.String(message).getBytes("UTF-8"));
+            channel.send(src, new java.net.InetSocketAddress(address, PORT));
+            
+            channel.receive(buffer);
+            
+            buffer.flip();
+            var received = decoder.decode(buffer).toString() + "";
+            buffer.clear();
+            
+            callback(received);
+        }catch(e){
+        	callback(null);
+        }
+    }}).start();
+}
+
+try{
+	Object.freeze(CustomPacket);
+	
+    channel = java.nio.channels.DatagramChannel.open();
+    decoder = java.nio.charset.Charset.forName("UTF-8").newDecoder();
+    buffer = java.nio.ByteBuffer.allocateDirect(65507);
     
-    return {
-    	connect: connect,
-        finalize: finalize
-    };
-}());
+    var scripts = net.zhuoweizhang.mcpelauncher.ScriptManager.scripts;
+    var ScriptableObject = org.mozilla.javascript.ScriptableObject;
+    
+	for(var i = 0; i < scripts.size(); i++) {
+		var scope = scripts.get(i).scope;
+		if(!ScriptableObject.hasProperty(scope, "CustomPacket")){
+			ScriptableObject.putProperty(scope, "CustomPacket", CustomPacket);
+		}
+	}
+}catch(e){
+    print(e.name + " - " + e.message);
+}
