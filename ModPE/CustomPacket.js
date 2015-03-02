32bbaa97e2fdb55a29c9cd08fb1b1466a20619a1
ChalkPE <amato0617@gmail.com>
2015-03-02 20:24:25 +0900

    라이센스 변경 및 문서화 주석 수정

--- ModPE/CustomPacket.js
+++ ModPE/CustomPacket.js
@@ -1,26 +1,91 @@
+/* Copyright 2014-2015 if(Team);
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 "use strict";
 
 /**
  * @author ChalkPE <amato17@naver.com>
  * @since 2014-11-03
+ * @copyright 2014-2015 if(Team);
+ * @license Apache-2.0
  * @see https://github.com/if-Team/CustomPacket
+ * @namespace
  */
-
 var CustomPacket = {};
+
+/**
+ * 통신에 사용할 포트입니다
+ * @memberof CustomPacket
+ * @type {Number}
+ * @const
+ */
 CustomPacket.PORT = 19131;
+
+/**
+ * 주기적으로 수신할 때의 딜레이 간격입니다
+ * @memberof CustomPacket
+ * @type {Number}
+ * @const
+ */
 CustomPacket.HEARTBEAT_SECONDS = 2;
+
+/**
+ * 주기적으로 수신할 때 서버에 전송하는 문자열입니다
+ * @memberof CustomPacket
+ * @type {String}
+ * @const
+ */
 CustomPacket.HEARTBEAT_SIGNAL = String.fromCharCode(16);
 
+/**
+ * 통신에 쓰이는 채널입니다
+ * @memberof CustomPacket
+ * @type {java.nio.channels.DatagramChannel}
+ * @private
+ */
 var channel = null;
+
+/**
+ * 서버로부터 수신한 바이트버퍼를 디코드할 디코더입니다
+ * @memberof CustomPacket
+ * @type {java.nio.charset.CharsetDecoder}
+ * @private
+ */
 var decoder = null;
+
+/**
+ * 수신에 쓰일 버퍼입니다
+ * @memberof CustomPacket
+ * @type {java.nio.ByteBuffer}
+ * @private
+ */
 var buffer = null;
 
 /**
- * @brief  서버에 연결합니다
- * @param  address {String} 서버의 IP
- * @param  message {String} 서버로 보내는 문자열
- * @param  callback {function} 응답을 받을 콜백 메서드 (String 또는 Error)
- * @return 없음
+ * 서버의 응답 결과를 받을 콜백입니다
+ * @callback Callback
+ * @param {String|Error} result - 응답 문자열 및 통신 중 발생한 에러
+ */
+
+/**
+ * 서버에 연결합니다
+ * @param {!String} address - 서버의 IP
+ * @param {!String} message - 서버로 보내는 문자열
+ * @param {!Callback} callback
+ * @memberof CustomPacket
+ * @public
  */
 CustomPacket.get = function get(address, message, callback){
     new java.lang.Thread({run: function(){
@@ -65,7 +130,14 @@ CustomPacket.get = function get(address, message, callback){
     }}).start();
 }
 
-CustomPacket.registerRefreshHook = function(address, callback){
+/**
+ * 2초에 한 번씩 지속적으로 서버에 연결합니다
+ * @param {!String} address - 서버의 IP
+ * @param {!Callback} callback
+ * @memberof CustomPacket
+ * @public
+ */
+CustomPacket.registerRepeatingHook = function(address, callback){
     try{
         new java.lang.Thread({run: function(){
             while(Server.getAddress() !== null){
@@ -79,15 +151,19 @@ CustomPacket.registerRefreshHook = function(address, callback){
 };
 
 try{
-	Object.freeze(CustomPacket);
-	
     channel = java.nio.channels.DatagramChannel.open();
     decoder = java.nio.charset.Charset.forName("UTF-8").newDecoder();
     buffer = java.nio.ByteBuffer.allocateDirect(65507);
+    
+	Object.freeze(CustomPacket);
 }catch(e){
-    print(e.name + " - " + e.message);
+    print(error.name + ": " + error.message + " at line " + error.lineNumber + "\n" + error.stack);
 }
 
+/**
+ * 맵에 입장할 때마다 라이브러리를 등록합니다
+ * @static
+ */
 function selectLevelHook(){
 	var scripts = net.zhuoweizhang.mcpelauncher.ScriptManager.scripts;
     var ScriptableObject = org.mozilla.javascript.ScriptableObject;
--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 
 A library which enables communication between ModPE clients and PocketMine servers
 
-This project has not been completely worked. 
+This project has not been completely worked.
 If you have any opinion about this project(or want to make pull request), we'll appreciate that :)
 ~~also for some English grammatical errors~~
 
-This project is under the terms of MIT License. See LICENSE text file in project root directory.
+This project is under the terms of Apache License 2.0. See LICENSE text file in project root directory.
+
 
 이 프로젝트는 아직 개발 중입니다. 
-프로젝트에 대한 의견/아이디어가 있거나, Pull request를 작성하신다면 기쁘게 받아들이겠습니다 :)
+프로젝트에 대한 의견 및 아이디어가 있거나, 풀 리쿼스트를 작성하신다면 기쁘게 받아들이겠습니다 :)
 
-이 프로젝트는 MIT 라이선스 하에서 개발/배포됩니다. LICENSE 파일에서 전문을 참고해 주세요.
+이 프로젝트는 Apache License 2.0 하에서 개발 및 배포됩니다. LICENSE 파일에서 전문을 참고해 주세요.
 
 ![alt text](http://i.imgur.com/zB5OQA3.png "Logo Title Text 1")
