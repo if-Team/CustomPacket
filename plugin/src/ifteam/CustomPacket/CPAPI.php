752b38572dce0cfa5622e886190b5bdd0e82b2d2
scalar77 <ska827@naver.com>
2015-07-12 02:25:31 +0900

    RakLib hooking succeeded

--- plugin/src/ifteam/CustomPacket/CPAPI.php
+++ plugin/src/ifteam/CustomPacket/CPAPI.php
@@ -5,26 +5,26 @@ namespace ifteam\CustomPacket;
 use pocketmine\Server;
 
 class CPAPI{
-    
-    private static $interface;
-    
-    public static function sendPacket(DataPacket $packet){
-        return MainLoader::getInterface()->sendPacket($packet);
-    }
-    
-    public static function matchPlayer($ip){
-        foreach(Server::getInstance()->getOnlinePlayers() as $p){
-            if($p->getAddress() === $ip) return $p;
-        }
-        return null;
-    }
-    
-    public static function blockAddress($address, $seconds){
-        MainLoader::getInterface()->blockAddress($address, $seconds);
-    }
-    
-    public static function unblockAddress($address){
-        MainLoader::getInterface()->unblockAddress($address);
-    }
-    
+
+	private static $interface;
+
+	public static function sendPacket(DataPacket $packet){
+		return MainLoader::getInterface()->sendPacket($packet);
+	}
+
+	public static function matchPlayer($ip){
+		foreach(Server::getInstance()->getOnlinePlayers() as $p){
+			if($p->getAddress() === $ip) return $p;
+		}
+		return null;
+	}
+
+	public static function blockAddress($address, $seconds){
+		MainLoader::getInterface()->blockAddress($address, $seconds);
+	}
+
+	public static function unblockAddress($address){
+		MainLoader::getInterface()->unblockAddress($address);
+	}
+
 }
--- plugin/src/ifteam/CustomPacket/MainLoader.php
+++ plugin/src/ifteam/CustomPacket/MainLoader.php
@@ -6,6 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use ifteam\CustomPacket\raknet\RakLibInterface;
 
 class MainLoader extends PluginBase implements Listener {
 	/** @var SocketInterface */
@@ -15,10 +16,16 @@ class MainLoader extends PluginBase implements Listener {
 		@mkdir($this->getDataFolder());
 		$this->saveDefaultConfig();
 
-		self::$interface = new SocketInterface($this->getServer(), $this->getConfig()->get("port", 19131));
+		/*self::$interface = new SocketInterface($this->getServer(), $this->getConfig()->get("port", 19131));
 
 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CustomPacketTask($this), 1);
-		$this->getLogger()->info("Registered CustomSocket tick schedule.");
+		$this->getLogger()->info("Registered CustomSocket tick schedule.");*/
+		$this->getLogger()->info("Shutting down RakLib to apply CustomPacket patch");
+		$i = $this->getServer()->getNetwork()->getInterfaces();
+		end($i)->shutdown();
+		$this->getServer()->getNetwork()->unregisterInterface(end($i));
+		$this->getLogger()->info("Reigstering modded Raklib with CustomPacket features");
+		$this->getServer()->getNetwork()->registerInterface(new RakLibInterface($this->getServer()));
 	}
 
 	public function onCommand(CommandSender $sender,Command $command, $label, Array $args){
